efe4003601c4280b3cd46b0c3c0f73d5db7f4992
Konrad Schmeink <konnikonradhd@gmail.com>
2025-05-04 23:57:02 +0200

    /stats fix

--- public/hamburgermenu.js
+++ public/hamburgermenu.js
@@ -1,27 +1,53 @@
 async function createHamburgerMenu() {
-    const me = await fetch('/me').then(r => r.json())
-
-    console.log(me);
-    
+    const me = await fetch('/me').then(r => r.json());
+    const path = window.location.pathname;
+    let links = [];
+  
+    if (me.username === 'public') {
+      // öffentlich (nicht eingeloggt)
+      links = [
+        `<a href="/login">Login</a>`,
+        `<a href="/signup">Register</a>`
+      ];
+    } else {
+      // eingeloggt
+      if (path === '/') {
+        // Root: Stats & Konrad
+        links = [
+          `<a href="/stats">Stats für ${me.username}</a>`,
+          `<a href="/konrad">Konrad</a>`
+        ];
+      } else if (path === '/konrad') {
+        // Konrad-Seite: Stats & Mom
+        links = [
+          `<a href="/stats">Stats für ${me.username}</a>`,
+          `<a href="/">Mom</a>`
+        ];
+      } else if (path === '/stats') {
+        // Stats-Seite: Mom & Konrad
+        links = [
+          `<a href="/">Mom</a>`,
+          `<a href="/konrad">Konrad</a>`
+        ];
+      } else {
+        // Fallback für alle anderen URLs
+        links = [
+          `<a href="/stats">Stats für ${me.username}</a>`,
+          `<a href="/">Mom</a>`,
+          `<a href="/konrad">Konrad</a>`
+        ];
+      }
+      // am Ende immer Logout
+      links.push(`<a href="/logout">Logout</a>`);
+    }
+  
     document.getElementById('burger-menu').innerHTML = `
-        <div class="bar"></div>
-        <div class="bar"></div>
-        <div class="bar"></div>
-        <div class="dropdown">
-            ${me.username === 'public' 
-            ? `
-                <a href="/login">Login</a>
-                <a href="/signup">Register</a>
-              `
-            : `
-                <a href="/stats">Stats für ${me.username}</a>
-                ${
-                    window.location.pathname === '/'
-                    ? `<a href="/konrad">Konrad</a>`
-                    : `<a href="/">Mom</a>`
-                }
-                <a href="/logout">Logout</a>
-            `}
-        </div>
-    `
-}
+      <div class="bar"></div>
+      <div class="bar"></div>
+      <div class="bar"></div>
+      <div class="dropdown">
+        ${links.join('')}
+      </div>
+    `;
+  }
+  
